3fe89d368cfa990f7ee7844c7364ce17e688b1fa
Vlad E <vladimir.hfs@gmail.com>
2019-07-28 12:32:16 -0700

    Removing duplicated import of RemoteWebDriver

--- src/main/java/facebook/Chrome.java
+++ src/main/java/facebook/Chrome.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.*;
--- src/main/java/facebook/Safari.java
+++ src/main/java/facebook/Safari.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.safari.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Safari {
 
